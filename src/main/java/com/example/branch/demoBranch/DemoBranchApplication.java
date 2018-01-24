637178f8cb17c3d5d3eaa7113b260a7c5564d6f1
Singh, Rahul <Rahul.Singh3@mastercard.com>
2018-01-24 15:13:42 +0530

    first commit

---
+++ src/main/java/com/example/branch/demoBranch/DemoBranchApplication.java
@@ -0,0 +1,12 @@
+package com.example.branch.demoBranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBranchApplication.class, args);
+	}
+}
